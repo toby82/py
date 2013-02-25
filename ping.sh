bc86c032d4da8ca9d4c3b2073d228328d1c0def0
root <root@HA01-110.(none)>
2013-02-25 11:28:11 +0800

    change trap

--- ping.sh
+++ ping.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 uphosts=./uphosts.`date +%m%d%Y`
 downhost=./downhosts.`date +%m%d%Y`
-profix=192.168.1
+#profix=$1
+read -p "please input(example:192.168.1): " profix
 for i in `seq 1 254`
 do
 	echo -en "pinging ${profix}.${i}......"
+	trap 'exit 0' 2 15
 	ping -c1 -w1 ${profix}.${i} > /dev/null 2>&1
 	if [ "$?" -eq "0" ]; then
 		echo " ok"
