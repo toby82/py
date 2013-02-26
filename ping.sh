432cc6863fd8b54b82286be102037024771e0710
root <root@rr-desktop.(none)>
2013-02-26 22:49:41 +0800

    change range ip

--- ping.sh
+++ ping.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+TMOUT=10
 uphosts=./uphosts.`date +%m%d%Y`
 downhost=./downhosts.`date +%m%d%Y`
 #profix=$1
-read -p "please input(example:192.168.1): " profix
-for i in `seq 1 254`
+read -p "please input(example:192.168.1.1-100): " p
+[ -z "$p" ] && exit 0
+ip=${p%-*}
+pre=${ip%.*}
+begin=${ip##*.}
+end=${p#*-}
+echo
+for i in `seq $begin $end`
 do
-	echo -en "pinging ${profix}.${i}......"
+	echo -en "pinging ${pre}.${i}......"
 	trap 'exit 0' 2 15
-	ping -c1 -w1 ${profix}.${i} > /dev/null 2>&1
+	ping -c1 -w1 ${pre}.${i} > /dev/null 2>&1
 	if [ "$?" -eq "0" ]; then
 		echo " ok"
-		echo "${profix}.${i}" >> $uphosts
+		echo "${pre}.${i}" >> $uphosts
 	else
 		echo " failed"
-		echo "${profix}.${i}" >> $downhost
+		echo "${pre}.${i}" >> $downhost
 	fi
 done
